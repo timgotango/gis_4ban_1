ff815891064bc8b63f270cb10954f17e9ee1a932
timgotango <xodzm3131@gmail.com>
2021-07-09 11:51:45 +0900

    7/9 views.py 에서 GET, POST 방식

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == "POST": # request 안에 method라는 데이터가 POST 방식이라면,
+        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!'}) # key는 text, value에는 문장을 넣었다
+    else: # GET 방식이라면,
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!'})
