0e7710f655df365204e01d3a77a0bff4219c72be
timgotango <xodzm3131@gmail.com>
2021-07-13 11:13:09 +0900

    7/13 NewModel.objects.all()

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from accountapp.models import NewModel
 
-# Create your views here.
 def hello_world(request):
     if request.method == "POST": # request 안에 method라는 데이터가 POST 방식이라면,
-        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!'}) # key는 text, value에는 문장을 넣었다
+
+        temp = request.POST.get('input_text') # request 안에 있는 POST 데이터 중, input_text 라는 이름을 가져와~~
+
+        new_model = NewModel() # models.py의 NewModel 클래스 객체 생성
+        new_model.text = temp # NewModel 안 text에 input한 것을 넣는 것.
+        new_model.save() # 저장까지 해줘야!
+
+        data_list = NewModel.objects.all()  # NewModel 데이터의 모든 값들
+        return render(request, 'accountapp/hello_world.html', context={'data_list': data_list}) # 최종적으로 저장된 것이 템플릿으로 넘어가는 것!!!
     else: # GET 방식이라면,
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!'})
+        data_list = NewModel.objects.all()  # NewModel 데이터의 모든 값들
+        return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
