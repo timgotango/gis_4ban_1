3f10457d3a40bb78d353313996032ca0b9bd5fa3
timgotango <xodzm3131@gmail.com>
2021-07-13 12:36:16 +0900

    7/13 CRUD

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
-app_name = 'accountapp'
+app_name = 'accountapp'              # accountapp 이름을 적어주어 다른 파일에서 라우팅해줄 수 있는 것.
 
-urlpatterns = {
+# name 인자를 기반으로 다른 파일에서 라우팅해줄 수 있는 것.
+# path 인 hello_world/를 가져와야하므로 views.py에서 reverse 함수 사용!!
+urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
-}
+    path('create/', AccountCreateView.as_view(), name='create'),
+]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,10 @@
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
+
 from accountapp.models import NewModel
 
 def hello_world(request):
@@ -11,8 +16,16 @@ def hello_world(request):
         new_model.text = temp # NewModel 안 text에 input한 것을 넣는 것.
         new_model.save() # 저장까지 해줘야!
 
-        data_list = NewModel.objects.all()  # NewModel 데이터의 모든 값들
-        return render(request, 'accountapp/hello_world.html', context={'data_list': data_list}) # 최종적으로 저장된 것이 템플릿으로 넘어가는 것!!!
+        # 보여지는 작업을 GET 으로 넘겨주기
+        # reverse 함수 : urls.py에서 name 인자를 가져온 후 path로 역으로 가져온다
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))  # Redirect
+
     else: # GET 방식이라면,
         data_list = NewModel.objects.all()  # NewModel 데이터의 모든 값들
         return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
+
+class AccountCreateView(CreateView): # 장고의 뷰의 제너릭에서 CreateView import
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world') # 함수가 아니라 클래스에서 사용하기 때문에 reverse_lazy 사용
+    template_name = 'accountapp/create.html' # 곧 만들 create.html
