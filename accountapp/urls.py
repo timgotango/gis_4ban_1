03e6a2f89eba7472649449f29dc076293e11f2f9
timgotango <xodzm3131@gmail.com>
2021-07-20 11:50:21 +0900

    7/20 delete view 로직, 생성

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = 'accountapp'              # accountapp 이름을 적어주어 다른 파일에서 라우팅해줄 수 있는 것.
 
@@ -14,4 +14,5 @@ urlpatterns = [
     path('create/', AccountCreateView.as_view(), name='create'), # create는 없는 것을 만드는 것
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'), # 다만 detail은 주소<int:pk>를 넘겨받아야!
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'), # 정보 수정이므로 user 키값 받아야.
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'), # 회원 정보 삭제이므로 마찬가지로 pk 값 받아야
 ]
