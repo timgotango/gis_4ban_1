44e6b581664ce7dc53f6b11dd5fa999be7a8377f
timgotango <xodzm3131@gmail.com>
2021-07-21 18:14:40 +0900

    7/21 decorator 적용

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,32 +1,33 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.forms import AccountCreationForm
 from accountapp.models import NewModel
 
+@login_required(login_url=reverse_lazy('accountapp:login'))
+# 장고에서 제공해주는 데코레이터(account/login으로 알아서 가준다 하지만 다르게 했다면, login_url로 redirect 가능!!)
 def hello_world(request):
-    if request.user.is_authenticated: # 로그인이 되어있다면~
-        if request.method == "POST": # request 안에 method라는 데이터가 POST 방식이라면,
+    if request.method == "POST": # request 안에 method라는 데이터가 POST 방식이라면,
 
-            temp = request.POST.get('input_text') # request 안에 있는 POST 데이터 중, input_text 라는 이름을 가져와~~
+        temp = request.POST.get('input_text') # request 안에 있는 POST 데이터 중, input_text 라는 이름을 가져와~~
 
-            new_model = NewModel() # models.py의 NewModel 클래스 객체 생성
-            new_model.text = temp # NewModel 안 text에 input한 것을 넣는 것.
-            new_model.save() # 저장까지 해줘야!
+        new_model = NewModel() # models.py의 NewModel 클래스 객체 생성
+        new_model.text = temp # NewModel 안 text에 input한 것을 넣는 것.
+        new_model.save() # 저장까지 해줘야!
 
-            # 보여지는 작업을 GET 으로 넘겨주기
-            # reverse 함수 : urls.py에서 name 인자를 가져온 후 path로 역으로 가져온다
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))  # Redirect
+        # 보여지는 작업을 GET 으로 넘겨주기
+        # reverse 함수 : urls.py에서 name 인자를 가져온 후 path로 역으로 가져온다
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))  # Redirect
 
-        else: # GET 방식이라면,
-            data_list = NewModel.objects.all()  # NewModel 데이터의 모든 값들
-            return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
-    else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
+    else: # GET 방식이라면,
+        data_list = NewModel.objects.all()  # NewModel 데이터의 모든 값들
+        return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
 
 class AccountCreateView(CreateView): # 장고의 뷰의 제너릭에서 CreateView import
     model = User
@@ -39,6 +40,9 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'         # key
     template_name = 'accountapp/detail.html'    # 추후에 만들 detail.html
 
+# class 안에 있는 함수이므로 '메소드'이다. 따라서 메소드용 함수를 만들지, 또는 decorator 자체를 변환할지 정해야. 우린 후자로.
+@method_decorator(login_required, 'get')    # method_decorator로 decorator 메소드용으로도 사용 가능하게 해주는 데코레이터 적용. :get 메소드에 적용하겠다.
+@method_decorator(login_required, 'post')    # pot 메소드에 적용하겠다.
 class AccountUpdateView(UpdateView): # 내 정보 수정
     model = User
     form_class = AccountCreationForm
@@ -46,32 +50,11 @@ class AccountUpdateView(UpdateView): # 내 정보 수정
     success_url = reverse_lazy('accountapp:hello_world') # 원래는 detail 페이지로 가면 좋지만, 상세 페이지마다 pk가 다르므로 이건 나중에 해보자
     template_name = 'accountapp/update.html'
 
-    def get(self, request, *args, **kwargs):    # get 메소드를 override, 로그인 인증 과정 넣어보자
-        if request.user.is_authenticated and self.get_object() == request.user: # 파이썬 파일에서 self.get_object 하면 target_user이다.
-            return super().get(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()  # 안된다고 알리기
-
-    def post(self, request, *args, **kwargs):    # post 메소드를 override
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().post(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
+@method_decorator(login_required, 'get')    # get 메소드에 적용하겠다.
+@method_decorator(login_required, 'post')    # pot 메소드에 적용하겠다.
 class AccountDeleteView(DeleteView): # 삭제이므로 form_class 없어도 된다.
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/delete.html'
 
-    def get(self, request, *args, **kwargs):  # get 메소드를 override, 로그인 인증 과정 넣어보자
-        if request.user.is_authenticated and self.get_object() == request.user:  # 파이썬 파일에서 self.get_object 하면 target_user이다.
-            return super().get(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()  # 안된다고 알리기
-
-    def post(self, request, *args, **kwargs):  # post 메소드를 override
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().post(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
--- decorators.py
+++ decorators.py
@@ -12,3 +12,30 @@ def hello_world(input_text):
     print(input_text)
 
 hello_world('Hello World')
+
+# 삼각형 / 사각형 넓이 계산 함수
+# 입력값이 모두 양수인지 확인하고, 아닐 경우 Error 발생시키는 Decorator 작성 및 적용
+@decorator
+def triangle(height, width):
+    return height * width / 2
+
+@decorator
+def rectangle(height, width):
+    return height * width
+
+def decorator(func):
+    def decorated(height, width):
+        if height > 0 and height > 0:
+            return func(height, width)
+        else:
+            raise ValueError('양수를 입력하시오')
+    return decorated
+
+print(triangle(2,3))
+
+
+# User 클래스 작성
+# User 클래스 내 is_authenticated 변수 작성
+# User 객체를 넓이 함수 인자로 전달
+# is_authenticated 변수 확인하고 True 아닐 경우 Error 발생
+
