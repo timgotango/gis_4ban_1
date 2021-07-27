4457e9035a10e6a40bdb94162f37095a3e7f628c
timgotango <xodzm3131@gmail.com>
2021-07-27 11:39:25 +0900

    7/27 create.html 파일 생성, urls.py 라우팅, views.py 로직 정의

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,3 +1,11 @@
+from django.urls import path
+
+from profileapp.views import ProfileCreateView
+
+app_name = 'profileapp'     # 이렇게 app_name 설정해주어야 나중에 'profileapp:create' 와 같은 방식으로 정해줄 수 있음!!
+
 urlpatterns = [
+    path('create/', ProfileCreateView.as_view(), name='create'),
+
 
 ]
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')    # 우선 hello_world 페이지로
+    template_name = 'profileapp/create.html'    # 추후에 만들 html 파일
