1a13182e9f8842d8cf4b8c90df626a8c88131c43
timgotango <xodzm3131@gmail.com>
2021-07-30 12:30:27 +0900

    7/30 commit 04 : magic-grid github 따와서 가져오기

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from django.views.generic import TemplateView
+ 
+app_name='articleapp'   # articleapp의 templates에 접근하기 위해 설정해줌
 
 urlpatterns = [
-
+    path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
 ]
