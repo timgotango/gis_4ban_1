d7b5e89ca7f1b07e091ea828994d8dd35b19ead1
timgotango <xodzm3131@gmail.com>
2021-08-03 12:37:34 +0900

    8/3 commit 04 : article create.html 만들기

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from articleapp.forms import ArticleCreationForm
+from articleapp.models import Article
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    form_class = ArticleCreationForm
+    success_url = reverse_lazy('articleapp:list')   # 게시글의 디테일 페이지로 원래 가는게 좋지만 일단은
+    template_name = 'articleapp/create.html'
+
+    def form_valid(self, form):
+        form.instance.writer = self.request.user    # writer 변수에 request를 보내고 있는 user를 할당!
+        return super().form_valid(form)
