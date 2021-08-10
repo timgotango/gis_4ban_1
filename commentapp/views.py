74db10b59df68de99c73e8b80e32cca79261e45b
timgotango <xodzm3131@gmail.com>
2021-08-10 12:07:31 +0900

    8/10 commit 03 : comment app - views.py, urls.py, create.html

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from commentapp.forms import CommentCreationForm
+from commentapp.models import Comment
+
+
+class CommentCreateView(CreateView):
+    model = Comment
+    form_class = CommentCreationForm
+    template_name = 'commentapp/create.html'
+
+    def get_success_url(self):  # 그냥 success_url 인자로만은 하나의 페이지로밖에 못가기 때문!
+        return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
