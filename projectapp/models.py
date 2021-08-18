a52af513538a32eac2a259413c2a87e4f9888931
timgotango <xodzm3131@gmail.com>
2021-08-18 17:04:36 +0900

    8/18 commit 02 : project app 과 article app 연동시켜주기

--- projectapp/models.py
+++ projectapp/models.py
@@ -7,3 +7,6 @@ class Project(models.Model):
     description = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='project/', null=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    # str 형으로 호출했을 때 어떻게 출력해줄 것이냐~를 정의해주기
+    def __str__(self):
+        return f'{self.name}'
