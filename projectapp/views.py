4f73c7705d8fc8e142eab3828c2f4d39d018de91
timgotango <xodzm3131@gmail.com>
2021-08-18 17:41:19 +0900

    8/18 commit 02 : project app 과 article app 연동시켜주기(2)

--- projectapp/views.py
+++ projectapp/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic.list import MultipleObjectMixin
 
+from articleapp.models import Article
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
 
@@ -18,10 +20,15 @@ class ProjectCreateView(CreateView):
     def get_success_url(self):
         return reverse('projectapp:detail', kwargs={'pk': self.object.pk})
 
-class ProjectDetailView(DetailView):
+# 여러개의 object를 사용할 수 있는 mixin
+class ProjectDetailView(DetailView, MultipleObjectMixin):
     model = Project
     context_object_name = 'target_project'
     template_name = 'projectapp/detail.html'
+    paginate_by = 20
+    def get_context_data(self, **kwargs):
+        article_list = Article.objects.filter(project=self.object)
+        return super().get_context_data(object_list=article_list ,**kwargs)
 
 class ProjectListView(ListView):
     model = Project
