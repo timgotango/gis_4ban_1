5e4df2e6916cdc36ff5e199f528ea56a0a501a4e
timgotango <xodzm3131@gmail.com>
2021-08-20 18:15:49 +0900

    8/20 commit 02 : subscribe app

--- accountapp/views.py
+++ accountapp/views.py
@@ -37,7 +37,7 @@ class AccountCreateView(CreateView): # 장고의 뷰의 제너릭에서 CreateVi
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world') # 함수가 아니라 클래스에서 사용하기 때문에 reverse_lazy 사용
-    template_name = 'accountapp/create.html' # 곧 만들 create.html
+    template_name = 'accountapp/create.html' # 곧 만들 subscribe.html
 
     # def get_success_url(self):
     #     return reverse('accountapp:detail', self.object.pk) # accountapp이므로 바로 object.pk
