062cdc05fc5a06e1c7d407667463a1e738676fc5
timgotango <xodzm3131@gmail.com>
2021-08-25 17:43:16 +0900

    8/25 commit 02 : What you see is what you get 적용 (medium editor 오픈소스)

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,10 +1,14 @@
-
+from django import forms
 from django.forms import ModelForm
 
 from articleapp.models import Article
 
 
 class ArticleCreationForm(ModelForm):
+    # class : editable을 가지고 있는 속성의 textarea를 적용!!
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable',
+                                                           'style': 'min-height: 10rem;'
+                                                                    'text-align: left;'}))
     class Meta:
         model = Article
         fields = ['project', 'title', 'image', 'content']
